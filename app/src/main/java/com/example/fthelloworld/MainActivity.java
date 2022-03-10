12bd22fc3d91b07c67541dc3fced991fb243326d
Paulius <pauliuslas@gmail.com>
2022-03-10 14:11:01 +0200

    Added color button

--- app/src/main/java/com/example/fthelloworld/MainActivity.java
+++ app/src/main/java/com/example/fthelloworld/MainActivity.java
@@ -2,30 +2,27 @@ package com.example.fthelloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvMain;
-    private boolean isHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.tvMain = (TextView)findViewById(R.id.tvMain);
-        this.isHelloWorld = true;
     }
 
-    public void onBtnMainCLick(View view) {
-        if(this.isHelloWorld){
-            this.tvMain.setText("smth new");
-        }
-        else{
-            this.tvMain.setText("Hello World!");
-        }
-        this.isHelloWorld = !this.isHelloWorld;
+    public void onBtnMainSpausk(View view) {
+        TextView tvMain1 = (TextView)findViewById(R.id.tvMain);
+        tvMain1.setText("Puikiai atlikta užduotis");
+
+    }
+
+    public void onBtnMainColor(View view) {
+        TextView tvMain1 = (TextView) findViewById(R.id.tvMain);
+        tvMain1.setTextColor(Color.parseColor("#FF018786"));
     }
 }
